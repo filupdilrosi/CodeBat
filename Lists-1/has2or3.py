acaf0e02e6b088444e13f4dc5eb99f74a5f82dc9
Phillip DelRossi <101755335+filupdilrosi@users.noreply.github.com>
2022-10-17 15:53:39 -0400

    Create has2or3.py

---
+++ Lists-1/has2or3.py
@@ -0,0 +1,8 @@
+#Given an int array length 2, return True if it contains a 2 or a 3.
+def has23(nums):
+  if 2 in nums:
+    return True
+  elif 3 in nums:
+    return True
+  else: 
+    return False
